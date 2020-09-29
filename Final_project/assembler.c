0fff3db597faf3d88555079bf904fb122b4ca395
microuzi9797 <ken97971@yahoo.com.tw>
2020-09-30 02:08:04 +0800

    Hack_assembler

---
+++ Final_project/assembler.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function.h"
+
+int main(int argc,char *argv[])
+{
+	char *filename;
+	filename = malloc(strlen(argv[1] + 1));
+	strcpy(filename,argv[1]);
+	//open th file
+	FILE *file_ptr;
+	file_ptr = fopen(filename,"r");
+	//read the file and build file-list
+	Asm_node *asm_head;
+	asm_head = calloc(1,sizeof(Asm_node));
+	Symbol_node *symbol_head;
+	symbol_head = calloc(1,sizeof(Symbol_node));
+	add_default_symbol(symbol_head);
+	buildlist(asm_head,symbol_head,file_ptr);
+	fclose(file_ptr);
+	//change .asm to .hack
+	char *dot;
+	dot = strrchr(filename,'.');
+	*dot = '\0';
+	filename = realloc(filename,strlen(filename) + 6);
+	filename = strcat(filename,".hack");
+	//write the file
+	file_ptr = fopen(filename,"w");
+	outputlist(asm_head,symbol_head,file_ptr);
+	//end
+	free(filename);
+	fclose(file_ptr);
+	return 0;
+}
---
+++ Final_project/function.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTION
+#define FUNCTION
+//file.asm list
+typedef struct asm_node
+{
+	char *assembly;
+	unsigned int machine_code;
+	struct asm_node *next;
+}Asm_node;
+//symbol-list
+typedef struct symbol_node
+{
+	char *symbol;
+	int memory_location;
+	struct symbol_node *next;
+}Symbol_node;
+//initialization
+void add_default_symbol(Symbol_node *head);
+void add_to_symbol_list(Symbol_node *head,char *string,int value);
+//first pass
+void buildlist(Asm_node *head,Symbol_node *symbol_head,FILE *fp);
+char *readline(FILE* fp);
+char *deletecomments(char *string);
+char *deletespaces(char *string);
+//second pass
+void outputlist(Asm_node *head,Symbol_node *symbol_head,FILE *fp);
+int findsymbol(Symbol_node *head,char *string);
+unsigned int A_instruction(char *assembly);
+unsigned int C_instruction(char *assembly);
+void make_binary_string(unsigned int input,char *output,int bit_num);
+#endif
