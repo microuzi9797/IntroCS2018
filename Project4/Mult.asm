79bfb8dd62d4bbbf68d10bfeeb4276a23b219b1b
microuzi9797 <ken97971@yahoo.com.tw>
2020-09-30 02:02:58 +0800

    assembly programming

---
+++ Project4/Mult.asm
@@ -0,0 +1,36 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+    @ans
+    M=0
+    @R1
+    D=M
+    @count
+    M=D
+(LOOP)
+    @count
+    D=M
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @ans
+    M=M+D
+    @count
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @ans
+    D=M
+    @R2
+    M=D
+    @22
+    0;JMP
